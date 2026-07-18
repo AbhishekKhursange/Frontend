505248f36d0bd10f642afa29e5750b46e30537bb
AbhishekKhursange <abhishekkhursange139@gmail.com>
2026-07-18 19:44:11 +0530

    update backend URL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiURL = axios.create({
-    baseURL: "https://backend-j472.onrender.com",
+    baseURL: "https://cravings-backend-9c5w.onrender.com",
 });
 
 export default apiURL;
